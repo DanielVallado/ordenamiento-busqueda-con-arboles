c0094d24ce16533efcbc2da3027a2eb3a6e0a4ac
GilmerEsau_PootPiste <97563937+Esau-Piste@users.noreply.github.com>
2023-05-23 03:13:30 -0600

    Add files via upload

---
+++ Interfaz_proyceto/constantes/style.py
@@ -0,0 +1,32 @@
+BACKGROUND = "#121212"
+FONT = ("Arial", 16) #FUENTE DE LA LETRA
+COMPONENT = "#363636" #COLOR DE LOS COMPONENTES 
+TEXT = "#84C9FB" #COLOR DEL TEXTO
+
+
+#DICCIONARIO DE ESTILOS 
+LABEL = {
+    "font" : FONT,
+    "bg" : COMPONENT,
+    "fg" : TEXT 
+
+}
+
+BUTTON = {
+    "font" : FONT,
+    "bg" : COMPONENT,
+    "fg" : TEXT
+}
+ENTRADA = {
+    "font" : ("Arial", 12),
+    "bg" : COMPONENT,
+    "fg" : TEXT
+
+}
+SPINGBOX = {
+    "font" : ("Arial", 14),
+    "bg" : COMPONENT,
+    "fg" : TEXT
+
+
+}
